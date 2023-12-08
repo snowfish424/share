dc589789ea744d1fbe14e29445159bf06c018083
snowfish424 <34232861+snowfish424@users.noreply.github.com>
2023-12-08 12:12:41 +0800

    Add files via upload

---
+++ KafkaConfig.java
@@ -0,0 +1,49 @@
+package com.um.ibdi.comm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+
+@Configuration
+@EnableKafka
+@EnableKafkaStreams
+public class KafkaConfig {
+
+	/**
+	 * applicationId
+	 */
+	@Value(value = "${spring.kafka.streams.application-id}")
+	private String applicationId;
+
+	@Value(value = "${spring.kafka.bootstrap-servers}")
+	private String bootstrapAddress;
+
+	/**
+	 * kStreamsConfig
+	 * 
+	 * @return
+	 */
+	@Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+	KafkaStreamsConfiguration kStreamsConfig() {
+
+		Map<String, Object> props = new HashMap<>();
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
+		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		props.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, 209715200);
+		props.put(StreamsConfig.PRODUCER_PREFIX + "max.request.size", 209715200);
+
+		return new KafkaStreamsConfiguration(props);
+	}
+
+}
